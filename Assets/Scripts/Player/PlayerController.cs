8f0730e35828421c876eff0e361d463a00a3dcac
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-10-19 04:36:53 +0300

    Update movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,59 +5,41 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask groundLayers;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private Transform playerCamera;
 
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float sensitivity;
     [SerializeField] private float jumpForce = 5f;
 
-    private Vector3 _playerMovementInput;
-    private Vector2 _playerMouseInput;
-    private float _xRot;
-
-
-    private void Update()
+    private void FixedUpdate()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        var mouseX = Input.GetAxis("Mouse X");
-        var mouseY = Input.GetAxis("Mouse Y");
-
-        _playerMovementInput = new Vector3(horizontal, 0f, vertical);
-        _playerMouseInput = new Vector2(mouseX, mouseY);
-
         MovePlayer();
-        MovePlayerCamera();
+        JumpPlayer();
     }
 
     private void MovePlayer()
     {
-        var moveVector = transform.TransformDirection(_playerMovementInput) * speed;
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        
+        var direction = new Vector3(horizontal, 0f, vertical);
+        var moveVector = transform.TransformDirection(direction) * speed;
         rb.velocity = new Vector3(moveVector.x, rb.velocity.y, moveVector.z);
+    }
 
+    private void JumpPlayer()
+    {
         if (Input.GetButtonDown("Jump") && GroundedCheck())
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
         }
     }
 
-    private void MovePlayerCamera()
-    {
-        _xRot -= _playerMouseInput.y * sensitivity;
-        _xRot = Mathf.Clamp(_xRot, -90f, 90f);
-
-        transform.Rotate(0f, _playerMouseInput.x * sensitivity, 0f);
-        playerCamera.transform.localRotation = Quaternion.Euler(_xRot, 0f, 0f);
-    }
-    
     private bool GroundedCheck()
     {
         return Physics.CheckSphere(groundCheck.position, 0.5f, groundLayers, QueryTriggerInteraction.Ignore);
     }
-    
+
     private void OnApplicationFocus(bool hasFocus)
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-    
 }
